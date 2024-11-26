f0f9c2ec030497aa1f412d143e689401ec5271e5
danplanned <elihcnad@gmail.com>
2024-11-25 17:21:13 -0800

    completed up to task 6, working on task 7

--- Basta Fazoolin'/script.py
+++ Basta Fazoolin'/script.py
@@ -4,9 +4,36 @@ class Menu():
     self.items = items
     self.start_time = start_time
     self.end_time = end_time
+  def NameOfMenu(menu):
+    print(menu.name)  
 
-brunch = Menu("brunch", {
+
+brunch = Menu("brunch", 
+{
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
 }, "11am", "4pm"
 )
+
+early_bird = Menu("early_bird", 
+{
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+},
+"3pm","6pm"
+)
+
+dinner = Menu("dinner",
+{
+  'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+},
+"5pm", "11pm"
+)
+
+kids = Menu("kids",
+{
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+},
+"11am", "9pm"          
+)
+
+Menu.NameOfMenu(kids)
 
