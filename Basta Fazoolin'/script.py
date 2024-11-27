3bf5373c8718be29d25afd587f32c7fba1913b1d
danplanned <elihcnad@gmail.com>
2024-11-26 19:36:29 -0800

    created Franchise class

--- Basta Fazoolin'/script.py
+++ Basta Fazoolin'/script.py
@@ -2,11 +2,12 @@ class Menu():
   def __init__(self, name, items, start_time, end_time):
     self.name = name
     self.items = items
-    self.start_time = start_time
-    self.end_time = end_time
+    self.start_time = int(start_time)
+    self.end_time = int(end_time)
 
   def __repr__(self):
-    return self.name + "menu available from " + self.start_time + " to " + self.end_time
+    return f"{self.name} menu available from {self.start_time} to {self.end_time}"
+
 
 
   def CalculateBill(self, purchased_items):
@@ -14,12 +15,13 @@ class Menu():
       for item in purchased_items:
         TotalBill += self.items[item]
       return TotalBill
+# Class Menu ends here
 
 
 brunch = Menu("brunch", 
 {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
-}, "11am", "4pm"
+}, "1100", "1600"
 )
 
 print(brunch.CalculateBill(["pancakes", "home fries", "coffee"]))
@@ -28,7 +30,7 @@ early_bird = Menu("early_bird",
 {
   'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
 },
-"3pm","6pm"
+"1500","1800"
 )
 
 print(early_bird.CalculateBill(["salumeria plate", "mushroom ravioli (vegan)"]))
@@ -37,15 +39,48 @@ dinner = Menu("dinner",
 {
   'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
 },
-"5pm", "11pm"
+"1700", "2300"
 )
 
 kids = Menu("kids",
 {
   'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
 },
-"11am", "9pm"          
+"1100", "2100"          
 )
+
+##print(brunch)
+
+class Franchise():
+  def __init__(self, address, menus):
+    self.address = address
+    self.menus = menus
+
+  def __repr__(self):
+    return self.address
+
+  def available_menus(self, time):
+    menuAvailable = []
+    for menu in self.menus:
+      if menu.start_time <= time <= menu.end_time:
+                menuAvailable.append(menu)
+    return menuAvailable
+
+
+
+# Class Franchise ends here
+
+flagship_store = Franchise("1232 West End Road", [brunch, early_bird, dinner, kids])
+new_installment = Franchise("12 East Mulberry Street", [brunch, early_bird, dinner, kids])
+
+print(flagship_store)
+
+for menu in flagship_store.available_menus(1700):
+  print(menu)
+
+
+
+
 
 
 
